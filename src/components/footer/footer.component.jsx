ca8505c456a54a17b8ac914987ce02b54ec61a6c
Joye Nettles <joye.nettles@contractors.mailchimp.com>
2020-04-05 22:57:37 -0400

    Add footer

---
+++ src/components/footer/footer.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './footer.styles.scss';
+
+const Footer = () => (
+    <footer>
+        <h3>Website brought to you by Joye to the World</h3>
+    </footer>
+);
+
+export default Footer;
--- src/components/landing-page/landing-page.component.jsx
+++ src/components/landing-page/landing-page.component.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import './landing-page.styles.css';
+import Footer from "../footer/footer.component";
 
 const LandingPage = () => (
     <>
-        <div className="logo-container">
-            <h1>Surgical Cutz</h1>
-            <h2>by MidKnight</h2>
+        <div className="content">
+            <div className="logo-container">
+                <h1>Surgical Cutz</h1>
+                <h2>by MidKnight</h2>
+            </div>
+            <div className="button-container">
+                <button>Book Now</button>
+            </div>
         </div>
-        <div className="button-container">
-            <button>Book Now</button>
-        </div>
+        <Footer className="footer"/>
     </>
 );
 
