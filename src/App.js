6bcdbd55e8d5d1d6fcdad44fe394ec551b0e4a00
Joye Nettles <joye.nettles@contractors.mailchimp.com>
2020-04-05 21:01:10 -0400

    Add fonts

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import logo from './assets/white-logo.png';
+import logo from './assets/images/white-logo.png';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="logo" alt="logo" />
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Surgical Cutz</h1>
+                <h2>by MidKnight</h2>
+            </header>
+        </div>
+    );
 }
 
 export default App;
