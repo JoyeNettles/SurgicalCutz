5884a76ecf09a07def7664a4130ec3b97a0af800
Joye Nettles <joye.nettles@contractors.mailchimp.com>
2020-04-05 23:47:08 -0400

    Add background to header

--- src/components/landing-page/landing-page.component.jsx
+++ src/components/landing-page/landing-page.component.jsx
@@ -1,17 +1,14 @@
 import React from "react";
 import './landing-page.styles.css';
-import Footer from "../footer/footer.component";
 
 const LandingPage = () => (
     <>
-        <div className="content">
-            <div className="logo-container">
-                <h1>Surgical Cutz</h1>
-                <h2>by MidKnight</h2>
-            </div>
-            <div className="button-container">
-                <button>Book Now</button>
-            </div>
+        <div className="logo-container">
+            <div className="header-text"><h1>Surgical Cutz</h1></div>
+            <h2>by MidKnight</h2>
+        </div>
+        <div className="button-container">
+            <button>Book Now</button>
         </div>
     </>
 );
