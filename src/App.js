bbfc1eb0a80a8310967d7d9e55dd93d2f910b9bd
Joye Nettles <joye.nettles@contractors.mailchimp.com>
2020-04-05 21:35:09 -0400

    Switch to white background, add button

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import logo from './assets/images/white-logo.png';
 import './App.css';
+import LandingPage from "./components/landing-page/landing-page.component";
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <h1>Surgical Cutz</h1>
-                <h2>by MidKnight</h2>
-            </header>
-        </div>
+        <LandingPage />
     );
 }
 
---
+++ src/components/landing-page/landing-page.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './landing-page.styles.css';
+
+const LandingPage = () => (
+    <section className="landing-page-container">
+        <div className="logo-container">
+        <h1>Surgical Cutz</h1>
+        <h2>by MidKnight</h2>
+        </div>
+        <button className="btn hvr-grow-shadow">Book Now</button>
+    </section>
+);
+
+export default LandingPage;
