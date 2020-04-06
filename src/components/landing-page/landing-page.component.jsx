ffb5b63e1d5db9c33dc0a293ac1e0a36a877e18c
Joye Nettles <joye.nettles@contractors.mailchimp.com>
2020-04-05 21:45:16 -0400

    Add prices button

--- src/components/landing-page/landing-page.component.jsx
+++ src/components/landing-page/landing-page.component.jsx
@@ -4,10 +4,13 @@ import './landing-page.styles.css';
 const LandingPage = () => (
     <section className="landing-page-container">
         <div className="logo-container">
-        <h1>Surgical Cutz</h1>
-        <h2>by MidKnight</h2>
+            <h1>Surgical Cutz</h1>
+            <h2>by MidKnight</h2>
         </div>
-        <button className="btn hvr-grow-shadow">Book Now</button>
+        <div className="button-container">
+            <button>Book Now</button>
+            <button>Prices</button>
+        </div>
     </section>
 );
 
